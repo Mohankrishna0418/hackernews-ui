06ea74976185b7ef3d626fa389b5560fec1471ee
Mohan Krishna A <mohankrishna.a757@gmail.com>
2025-05-08 16:14:26 +0530

    Update dependencies to include Radix UI components and TanStack Form

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,14 +1,9 @@
-// lib/integrations/better-auth/index.ts
-
 import { createAuthClient } from "better-auth/react";
+import { serverUrl } from "./evironment";
 import { nextCookies } from "better-auth/next-js";
-import { usernameClient } from "better-auth/client/plugins";
-
-export const url =
-  // "https://hackernews.lemonisland-20d31e0a.centralindia.azurecontainerapps.io";
-  "http://localhost:3000";
 
 export const auth = createAuthClient({
-  baseURL: `${url}`,
-  plugins: [usernameClient(), nextCookies()],
+  baseURL: serverUrl,
+  basePath: "/authentications",
+  plugins: [nextCookies()],
 });
